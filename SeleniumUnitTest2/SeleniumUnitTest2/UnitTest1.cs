3482685f4a452f6a3a340350af64b2d5d19ec75c
Kurt Student <kurtstudent@outlook.com>
2018-02-19 13:38:04 -0800

    checking in selenium tests

---
+++ SeleniumUnitTest2/SeleniumUnitTest2/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumUnitTest2
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.IE;
+    using OpenQA.Selenium.Remote;
+    using OpenQA.Selenium.PhantomJS;
+    using System;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        private string baseURL = "https://www.google.com";
+        private RemoteWebDriver driver;
+        private string browser;
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [Priority(1)]
+        [Owner("Chrome")]
+        public void TireSearch_Any()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Navigate().GoToUrl(this.baseURL);
+            driver.FindElementById("lst-ib").Clear();
+            driver.FindElementById("lst-ib").SendKeys("tire");
+            string x = driver.Title;
+            //do other Selenium things here!
+            Assert.AreEqual("Google", x);
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+        }
+    }
+}
